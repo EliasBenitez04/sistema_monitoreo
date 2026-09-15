d62bce66a1267407d5e05c9a61010a1d137adc9a
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-15 15:01:50 -0300

    refactor(ui): modernize clientes index

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,75 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    {{-- Encabezado de la página --}}
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
+    <x-page-header
+        title="Clientes"
+        subtitle="Administración centralizada de clientes y datos comerciales."
+        icon="fas fa-user-friends">
+        <a href="{{ route('clientes.create') }}" class="btn btn-primary">
+            <i class="fas fa-plus"></i>
+            Nuevo cliente
+        </a>
+    </x-page-header>
 
-                {{-- Título --}}
-                <div class="col-sm-6">
-                    <h1>
-                        <i class="fas fa-users"></i>
-                        Listado Clientes
-                    </h1>
-                </div>
-
-                {{-- Breadcrumb --}}
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-
-                        <li class="breadcrumb-item">
-                            <a href="{{ url('/home') }}">
-                                <i class="fas fa-home"></i>
-                                Inicio
-                            </a>
-                        </li>
-
-                        <li class="breadcrumb-item active">
-                            Clientes
-                        </li>
-
-                    </ol>
-                </div>
-
-            </div>
-        </div>
-    </section>
-
-
-    {{-- Contenido --}}
     <div class="content px-3">
-
-        {{-- Alertas --}}
         @include('sweetalert::alert')
 
-        <div class="clearfix"></div>
-
-
-        {{-- Card de clientes --}}
-        <div class="card">
-
-            {{-- Encabezado de la card --}}
+        <div class="card sm-data-card">
             <div class="card-header">
-                <h3 class="card-title">
-                    <i class="fas fa-list"></i>
-                    Clientes registrados
-                </h3>
-
-                <div class="card-tools">
-                    <a href="{{ route('clientes.create') }}" class="btn btn-primary btn-sm">
-                        <i class="fas fa-plus"></i>
-                        Agregar Nuevo
-                    </a>
+                <div>
+                    <h3 class="card-title mb-0">Clientes registrados</h3>
+                    <small class="text-muted">Información disponible para pedidos y procesos comerciales.</small>
                 </div>
             </div>
-
-            {{-- Tabla --}}
             <div class="card-body p-0">
                 @include('clientes.table')
             </div>
-
         </div>
-
     </div>
 @endsection
