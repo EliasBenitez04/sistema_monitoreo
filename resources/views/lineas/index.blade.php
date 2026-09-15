d973595808f0a3c4a75bbd4a962deb998feec0c9
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-15 15:02:05 -0300

    refactor(ui): modernize lineas index

--- resources/views/lineas/index.blade.php
+++ resources/views/lineas/index.blade.php
@@ -1,29 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>Lineas</h1>
-                </div>
-                <div class="col-sm-6">
-                    <a class="btn btn-primary float-right" href="{{ route('lineas.create') }}">
-                        Nuevo
-                    </a>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Líneas"
+        subtitle="Clasificación de productos para catálogo, análisis y seguimiento."
+        icon="fas fa-tags">
+        <a class="btn btn-primary" href="{{ route('lineas.create') }}">
+            <i class="fas fa-plus"></i>
+            Nueva línea
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
-
         @include('sweetalert::alert')
 
-        <div class="clearfix"></div>
-
-        <div class="card">
-            @include('lineas.table')
+        <div class="card sm-data-card">
+            <div class="card-header">
+                <div>
+                    <h3 class="card-title mb-0">Líneas registradas</h3>
+                    <small class="text-muted">Organice las familias utilizadas por los módulos de producto y monitoreo.</small>
+                </div>
+            </div>
+            <div class="card-body p-0">
+                @include('lineas.table')
+            </div>
         </div>
     </div>
 @endsection
