02c8837181bd5b3c272ba0c0b0247109609f2f01
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-15 10:06:24 -0300

    style: modernize sidebar branding

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,37 +1,17 @@
-<aside class="main-sidebar sidebar-dark-darkest elevation-4">
-    <a href="{{ route('home') }}" class="brand-link text-center">
-        <img src="{{ asset('storage/logos/gts_logo.jpg') }}" alt="Gotitas" class="brand-image img-circle elevation-3"
-            style="opacity: .9">
-
-        <span class="brand-text font-weight-bold text">
-            {{ config('app.name') }}
+<aside class="main-sidebar sidebar-dark-primary elevation-0">
+    <a href="{{ route('home') }}" class="brand-link">
+        <img src="{{ asset('storage/logos/gts_logo.jpg') }}" alt="{{ config('app.name') }}" class="brand-image sm-brand-logo">
+        <span class="sm-brand-copy">
+            <span class="sm-brand-name">{{ config('app.name') }}</span>
+            <span class="sm-brand-caption">Gestión operativa</span>
         </span>
     </a>
 
     <div class="sidebar">
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                data-accordion="false">
+        <nav>
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 @include('layouts.menu')
             </ul>
         </nav>
     </div>
-    <style>
-        .sidebar-dark-darkest {
-            background-color: #000000;
-            /* casi negro */
-            color: #f9fbff;
-        }
-
-        .sidebar-dark-darkest .nav-sidebar .nav-link.active {
-            background-color: #0a60ff;
-            /* mantiene azul para activo */
-            color: #fff;
-        }
-
-        .sidebar-dark-darkest .nav-sidebar .nav-link:hover {
-            background-color: #313131;
-            color: #fff;
-        }
-    </style>
 </aside>
