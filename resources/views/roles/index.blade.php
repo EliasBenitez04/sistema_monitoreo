a764ed582d333f04819ca4a1ee7fd40f25366f77
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-15 15:02:40 -0300

    refactor(ui): modernize roles index

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,31 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>Roles</h1>
-                </div>
-                <div class="col-sm-6">
-                    @can('roles create')
-                        <a class="btn btn-primary float-right" href="{{ route('roles.create') }}">
-                            Nuevo Rol
-                        </a>
-                    @endcan
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Roles"
+        subtitle="Defina perfiles de acceso y agrupe permisos según responsabilidades."
+        icon="fas fa-user-shield">
+        @can('roles create')
+            <a class="btn btn-primary" href="{{ route('roles.create') }}">
+                <i class="fas fa-plus"></i>
+                Nuevo rol
+            </a>
+        @endcan
+    </x-page-header>
 
     <div class="content px-3">
-
         @include('sweetalert::alert')
 
-        <div class="clearfix"></div>
-
-        <div class="card">
-            @include('roles.table')
+        <div class="card sm-data-card">
+            <div class="card-header">
+                <div>
+                    <h3 class="card-title mb-0">Roles configurados</h3>
+                    <small class="text-muted">Controle qué funciones puede utilizar cada perfil del sistema.</small>
+                </div>
+            </div>
+            <div class="card-body p-0">
+                @include('roles.table')
+            </div>
         </div>
     </div>
 @endsection
