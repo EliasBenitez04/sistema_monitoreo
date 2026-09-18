cc0c337e9bd4f1e33410e3c995fe7d8c9514fdb3
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:11:56 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/stock_ventas_sucursales/edit.blade.php
+++ resources/views/stock_ventas_sucursales/edit.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
+@section('title', 'Editar carga de ventas y stock | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                        Edit Stock Ventas Sucursales
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Editar carga de ventas y stock"
+        subtitle="Actualice la información de la carga."
+        icon="fas fa-chart-line">
+        <a href="{{ route('stock_ventas_sucursales.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
 
         @include('adminlte-templates::common.errors')
 
-        <div class="card">
+        <div class="card sm-form-card">
 
             {!! Form::model($stockVentasSucursales, ['route' => ['stockVentasSucursales.update', $stockVentasSucursales->id], 'method' => 'patch']) !!}
 
@@ -27,7 +27,7 @@
                 </div>
             </div>
 
-            <div class="card-footer">
+            <div class="card-footer d-flex justify-content-end">
                 {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
                 <a href="{{ route('stockVentasSucursales.index') }}" class="btn btn-default"> Cancel </a>
             </div>
