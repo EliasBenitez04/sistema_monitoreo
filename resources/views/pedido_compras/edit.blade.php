2af3c534b8998cd9cbe760757b3a16256287c190
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:10:57 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/pedido_compras/edit.blade.php
+++ resources/views/pedido_compras/edit.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
+@section('title', 'Editar pedido de compra | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                        Editar Pedido Compras
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Editar pedido de compra"
+        subtitle="Actualice la información del pedido."
+        icon="fas fa-shopping-cart">
+        <a href="{{ route('pedido_compras.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
 
         @include('adminlte-templates::common.errors')
 
-        <div class="card">
+        <div class="card sm-form-card">
 
             {!! Form::model($pedido_compras, [
                 'route' => ['pedido_compras.update', $pedido_compras->id_pedido],
@@ -42,9 +42,9 @@
                 </div>
             </div>
 
-            <div class="card-footer">
-                {!! Form::submit('Actualizar', ['class' => 'btn btn-success']) !!}
-                <a href="{{ route('pedido_compras.index') }}" class="btn btn-primary"> Cancelar </a>
+            <div class="card-footer d-flex justify-content-end">
+                {!! Form::submit('Actualizar', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('pedido_compras.index') }}" class="btn btn-default ml-2">Cancelar</a>
             </div>
 
             {!! Form::close() !!}
