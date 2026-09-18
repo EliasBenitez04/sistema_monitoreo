030108da041746ed3d6e1514f2581a6951c5fbd3
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:10:30 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/lineas/edit.blade.php
+++ resources/views/lineas/edit.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
+@section('title', 'Editar línea | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                        Editar Linea
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Editar línea"
+        subtitle="Actualice la información de la línea."
+        icon="fas fa-tags">
+        <a href="{{ route('lineas.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
 
         @include('adminlte-templates::common.errors')
 
-        <div class="card">
+        <div class="card sm-form-card">
 
             {!! Form::model($linea, ['route' => ['lineas.update', $linea->linea_cod], 'method' => 'patch', 'data-confirm' => 'confirm-submit']) !!}
 
@@ -28,9 +28,9 @@
                 </div>
             </div>
 
-            <div class="card-footer">
-                {!! Form::submit('Actualizar', ['class' => 'btn btn-success']) !!}
-                <a href="{{ route('lineas.index') }}" class="btn btn-primary"> Cancelar </a>
+            <div class="card-footer d-flex justify-content-end">
+                {!! Form::submit('Actualizar', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('lineas.index') }}" class="btn btn-default ml-2">Cancelar</a>
             </div>
 
             {!! Form::close() !!}
