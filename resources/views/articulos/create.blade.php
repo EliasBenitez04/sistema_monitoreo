544efb33d9a056864e747f9887189e5e84dbd210
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:09:00 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/articulos/create.blade.php
+++ resources/views/articulos/create.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
+@section('title', 'Nuevo artículo | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                        Crear Articulos
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Nuevo artículo"
+        subtitle="Registre la información del artículo."
+        icon="fas fa-box-open">
+        <a href="{{ route('articulos.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
 
         @include('adminlte-templates::common.errors')
 
-        <div class="card">
+        <div class="card sm-form-card">
 
             {!! Form::open(['route' => 'articulos.store','class' => 'confirm-submit' ]) !!}
 
@@ -29,9 +29,9 @@
                 </div>
             </div>
 
-            <div class="card-footer">
-                {!! Form::submit('Guardar', ['class' => 'btn btn-success']) !!}
-                <a href="{{ route('articulos.index') }}" class="btn btn-primary"> Cancelar </a>
+            <div class="card-footer d-flex justify-content-end">
+                {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('articulos.index') }}" class="btn btn-default ml-2">Cancelar</a>
             </div>
 
             {!! Form::close() !!}
