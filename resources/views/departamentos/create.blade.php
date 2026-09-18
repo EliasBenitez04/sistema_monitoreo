c865846928ca62679074e8e203bcede7a20d9ceb
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:10:09 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/departamentos/create.blade.php
+++ resources/views/departamentos/create.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
+@section('title', 'Nuevo departamento | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                    Crear Nuevo Departamento
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Nuevo departamento"
+        subtitle="Registre un departamento para la gestión territorial."
+        icon="fas fa-map-marked-alt">
+        <a href="{{ route('Departamentos.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
 
         @include('adminlte-templates::common.errors')
 
-        <div class="card">
+        <div class="card sm-form-card">
 
             {!! Form::open(['route' => 'Departamentos.store','class' => 'confirm-submit']) !!}
 
@@ -33,10 +33,10 @@
 
             </div>
 
-            <div class="card-footer">
+            <div class="card-footer d-flex justify-content-end">
 
-                {!! Form::submit('Guardar', ['class' => 'btn btn-success']) !!}
-                <a href="{{ route('Departamentos.index') }}" class="btn btn-primary"> Cancelar </a>
+                {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('Departamentos.index') }}" class="btn btn-default ml-2">Cancelar</a>
 
             </div>
 
