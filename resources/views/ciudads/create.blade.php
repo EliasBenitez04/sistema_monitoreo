d465e1f3763905223b2c4a6c400175b8ad4f7aa8
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:09:38 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/ciudads/create.blade.php
+++ resources/views/ciudads/create.blade.php
@@ -1,21 +1,23 @@
 @extends('layouts.app')
 
+@section('title', 'Nueva ciudad | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>Crear Ciudad</h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Nueva ciudad"
+        subtitle="Registre una ciudad para la gestión territorial."
+        icon="fas fa-city">
+        <a href="{{ route('ciudades.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
 
         @include('adminlte-templates::common.errors')
 
-        <div class="card">
+        <div class="card sm-form-card">
 
             {!! Form::open(['route' => 'ciudades.store', 'class' => 'confirm-submit']) !!}
 
@@ -28,9 +30,9 @@
                 </div>
             </div>
 
-            <div class="card-footer">
-                {!! Form::submit('Guardar', ['class' => 'btn btn-success']) !!}
-                <a href="{{ route('ciudades.index') }}" class="btn btn-primary">Cancelar</a>
+            <div class="card-footer d-flex justify-content-end">
+                {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('ciudades.index') }}" class="btn btn-default ml-2">Cancelar</a>
             </div>
 
             {!! Form::close() !!}
