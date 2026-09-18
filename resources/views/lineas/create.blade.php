981bd5951e5ef81a4240a071184e182d43ea33d5
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:10:19 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/lineas/create.blade.php
+++ resources/views/lineas/create.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
+@section('title', 'Nueva línea | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                        Crear Nueva Lineas
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Nueva línea"
+        subtitle="Registre una línea de productos."
+        icon="fas fa-tags">
+        <a href="{{ route('lineas.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
 
         @include('adminlte-templates::common.errors')
 
-        <div class="card">
+        <div class="card sm-form-card">
 
             {!! Form::open(['route' => 'lineas.store', 'class' => 'confirm-submit']) !!}
 
@@ -33,9 +33,9 @@
 
             </div>
 
-            <div class="card-footer">
-                {!! Form::submit('Guardar', ['class' => 'btn btn-success']) !!}
-                <a href="{{ route('lineas.index') }}" class="btn btn-primary"> Cancelar </a>
+            <div class="card-footer d-flex justify-content-end">
+                {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('lineas.index') }}" class="btn btn-default ml-2">Cancelar</a>
             </div>
 
             {!! Form::close() !!}
