a6b88829bf1836b6c84c9cfda99e63ff86ab9295
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:09:17 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/auditorias/show.blade.php
+++ resources/views/auditorias/show.blade.php
@@ -1,26 +1,20 @@
 @extends('layouts.app')
 
+@section('title', 'Detalle de auditoría | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>
-                    @lang('models/auditorias.singular') @lang('crud.detail')
-                    </h1>
-                </div>
-                <div class="col-sm-6">
-                    <a class="btn btn-default float-right"
-                       href="{{ route('auditorias.index') }}">
-                                                    @lang('crud.back')
-                                            </a>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Detalle de auditoría"
+        subtitle="Información registrada de la auditoría."
+        icon="fas fa-clipboard-check">
+        <a href="{{ route('auditorias.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
-        <div class="card">
+        <div class="card sm-detail-card">
             <div class="card-body">
                 <div class="row">
                     @include('auditorias.show_fields')
