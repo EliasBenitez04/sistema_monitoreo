f64d8c265ff6760a325231a85f545ff20ab738fe
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:09:50 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -1,31 +1,29 @@
 @extends('layouts.app')
 
+@section('title', 'Nuevo cliente | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                        Crear Clientes
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Nuevo cliente"
+        subtitle="Registre la información comercial y de contacto."
+        icon="fas fa-user-friends">
+        <a href="{{ route('clientes.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
 
         @include('adminlte-templates::common.errors')
 
-        <div class="card">
+        <div class="card sm-form-card">
 
             {!! Form::open([
                 'route' => 'clientes.store',
                 'class' => 'confirm-submit',
                 'files' => true,
             ]) !!}
-
-            {!! Form::open(['route' => 'clientes.store','class' => 'confirm-submit' ]) !!}
 
             <div class="card-body">
                 @include('sweetalert::alert')
@@ -37,9 +35,9 @@
 
             </div>
 
-            <div class="card-footer">
-                {!! Form::submit('Guardar', ['class' => 'btn btn-success']) !!}
-                <a href="{{ route('clientes.index') }}" class="btn btn-primary"> Cancelar </a>
+            <div class="card-footer d-flex justify-content-end">
+                {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('clientes.index') }}" class="btn btn-default ml-2">Cancelar</a>
             </div>
 
             {!! Form::close() !!}
