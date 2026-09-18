6e6e6e032fe34ac9c9cd1347c57cf4e31f37bb26
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:25:23 -0300

    refactor(ui): normaliza edicion de clientes

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -1,42 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                        Editar Cliente
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Editar cliente"
+        subtitle="Actualice la información comercial y de contacto."
+        icon="fas fa-user-edit">
+        <a href="{{ route('clientes.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
-
         @include('adminlte-templates::common.errors')
+        @include('sweetalert::alert')
 
-        <div class="card">
-
-            {!! Form::model($cliente, ['route' => ['clientes.update', $cliente->id_cliente], 'method' => 'patch' ,'class' => 'confirm-submit']) !!}
+        <div class="card sm-form-card">
+            {!! Form::model($cliente, [
+                'route' => ['clientes.update', $cliente->id_cliente],
+                'method' => 'patch',
+                'class' => 'confirm-submit',
+            ]) !!}
 
             <div class="card-body">
-
-                @include('sweetalert::alert')
-
                 <div class="row">
                     @include('clientes.fields')
                 </div>
             </div>
 
-            <div class="card-footer">
-                {!! Form::submit('Actualizar', ['class' => 'btn btn-success']) !!}
-                <a href="{{ route('clientes.index') }}" class="btn btn-primary"> Cancelar </a>
+            <div class="card-footer d-flex justify-content-end flex-wrap">
+                <a href="{{ route('clientes.index') }}" class="btn btn-default">
+                    Cancelar
+                </a>
+                {!! Form::submit('Actualizar cliente', ['class' => 'btn btn-primary']) !!}
             </div>
 
             {!! Form::close() !!}
-
         </div>
     </div>
 @endsection
