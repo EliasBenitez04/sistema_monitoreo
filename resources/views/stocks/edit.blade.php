374426274c8fc7f855429d998ad287c0ccf78c5f
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:12:06 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/stocks/edit.blade.php
+++ resources/views/stocks/edit.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
+@section('title', 'Editar carga de stock | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                        Edit Stock
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Editar carga de stock"
+        subtitle="Actualice la información de la carga de stock."
+        icon="fas fa-boxes">
+        <a href="{{ route('stocks.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
 
         @include('adminlte-templates::common.errors')
 
-        <div class="card">
+        <div class="card sm-form-card">
 
             {!! Form::model($stock, ['route' => ['stocks.update', $stock->id], 'method' => 'patch']) !!}
 
@@ -27,7 +27,7 @@
                 </div>
             </div>
 
-            <div class="card-footer">
+            <div class="card-footer d-flex justify-content-end">
                 {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
                 <a href="{{ route('stocks.index') }}" class="btn btn-default"> Cancel </a>
             </div>
