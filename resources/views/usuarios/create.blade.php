fa5d7cea51fdaaaa61a54bb8a682f91e11ce2825
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:12:50 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/usuarios/create.blade.php
+++ resources/views/usuarios/create.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
+@section('title', 'Nuevo usuario | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                        Crear Nuevo Usuario
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Nuevo usuario"
+        subtitle="Registre un usuario y sus datos de acceso."
+        icon="fas fa-users">
+        <a href="{{ route('usuarios.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
 
         @include('adminlte-templates::common.errors')
 
-        <div class="card">
+        <div class="card sm-form-card">
 
             {!! Form::open(['route' => 'usuarios.store','class' => 'confirm-submit']) !!}
 
@@ -31,9 +31,9 @@
 
             </div>
 
-            <div class="card-footer">
-                {!! Form::submit('Guardar', ['class' => 'btn btn-success']) !!}
-                <a href="{{ route('usuarios.index') }}" class="btn btn-primary"> Cancelar </a>
+            <div class="card-footer d-flex justify-content-end">
+                {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('usuarios.index') }}" class="btn btn-default ml-2">Cancelar</a>
             </div>
 
             {!! Form::close() !!}
