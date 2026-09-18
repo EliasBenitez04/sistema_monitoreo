cad56f7044b620f1ddd3fea90f222bc6bd9907db
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:14:16 -0300

    refactor(ui): simplify lot management header

--- resources/views/redistribucion_sugeridas/lotes.blade.php
+++ resources/views/redistribucion_sugeridas/lotes.blade.php
@@ -1,76 +1,22 @@
 @extends('layouts.app')
 
+@section('title', 'Gestión de lotes | ' . config('app.name'))
+
 @section('content')
     @include('redistribucion_sugeridas.importar-remisiones')
+    <x-page-header
+        title="Gestión de lotes"
+        subtitle="Control y seguimiento de movimientos de redistribución."
+        icon="fas fa-layer-group">
+        @can('redistribucionsugerencia importarRemisiones')
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalImportarRemisiones">
+                <i class="fas fa-file-import"></i>
+                Importar remisiones
+            </button>
+        @endcan
+    </x-page-header>
 
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row align-items-center">
-
-                {{-- TÍTULO --}}
-                <div class="col-md-7">
-                    <div class="page-heading">
-
-                        <div class="page-heading-icon">
-                            <i class="fas fa-layer-group"></i>
-                        </div>
-
-                        <div>
-                            <h1>Gestión de Lotes</h1>
-
-                            <small>
-                                Control y seguimiento de movimientos de redistribución
-                            </small>
-                        </div>
-
-                    </div>
-                </div>
-
-
-                {{-- ACCIONES + BREADCRUMB --}}
-                <div class="col-md-5">
-
-                    {{-- BOTÓN IMPORTAR --}}
-                    @can('redistribucionsugerencia importarRemisiones')
-                        <div class="text-md-right mb-3">
-
-                            <button type="button" class="btn btn-import-remisiones" data-toggle="modal"
-                                data-target="#modalImportarRemisiones">
-
-                                <span class="import-btn-icon">
-                                    <i class="fas fa-file-import"></i>
-                                </span>
-
-                                <span>Importar remisiones</span>
-
-                            </button>
-                        </div>
-                    @endcan
-                    
-
-
-                    {{-- BREADCRUMB --}}
-                    <ol class="breadcrumb float-md-right mb-0">
-
-                        <li class="breadcrumb-item">
-                            <a href="{{ route('RedistribucionSugeridas.index') }}">
-                                Redistribución Sugerida
-                            </a>
-                        </li>
-
-                        <li class="breadcrumb-item active">
-                            Gestión de Lotes
-                        </li>
-
-                    </ol>
-
-                </div>
-
-            </div>
-        </div>
-    </section>
-
-    <section class="content">
+    <section class="content px-3">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-xl-3 col-md-6">
