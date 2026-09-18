a5d4f9f0ae5760b66fa86f7fe2fb7c476e9213f1
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:09:42 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/ciudads/edit.blade.php
+++ resources/views/ciudads/edit.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
+@section('title', 'Editar ciudad | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                        Editar Ciudad
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Editar ciudad"
+        subtitle="Actualice la información de la ciudad."
+        icon="fas fa-city">
+        <a href="{{ route('ciudades.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
 
         @include('adminlte-templates::common.errors')
 
-        <div class="card">
+        <div class="card sm-form-card">
 
             {!! Form::model($ciudades, ['route' => ['ciudades.update', $ciudades->id_ciudad], 'method' => 'patch']) !!}
 
@@ -29,9 +29,9 @@
                 </div>
             </div>
 
-            <div class="card-footer">
-                {!! Form::submit('Actualizar', ['class' => 'btn btn-success']) !!}
-                <a href="{{ route('ciudades.index') }}" class="btn btn-primary"> Cancelar </a>
+            <div class="card-footer d-flex justify-content-end">
+                {!! Form::submit('Actualizar', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('ciudades.index') }}" class="btn btn-default ml-2">Cancelar</a>
             </div>
 
             {!! Form::close() !!}
