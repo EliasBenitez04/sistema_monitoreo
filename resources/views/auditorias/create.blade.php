c1c63e6c1242aa7ad7c4cd956d583a127826f986
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:09:11 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/auditorias/create.blade.php
+++ resources/views/auditorias/create.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
+@section('title', 'Nueva auditoría | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                    Create Auditorias
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Nueva auditoría"
+        subtitle="Registre la información de la auditoría."
+        icon="fas fa-clipboard-check">
+        <a href="{{ route('auditorias.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
 
         @include('adminlte-templates::common.errors')
 
-        <div class="card">
+        <div class="card sm-form-card">
 
             {!! Form::open(['route' => 'auditorias.store']) !!}
 
@@ -29,7 +29,7 @@
 
             </div>
 
-            <div class="card-footer">
+            <div class="card-footer d-flex justify-content-end">
                 {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
                 <a href="{{ route('auditorias.index') }}" class="btn btn-default"> Cancel </a>
             </div>
