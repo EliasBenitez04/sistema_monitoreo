c2f11f2274f61344caef2e018cf5e1482c06d9d6
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 13:12:22 -0300

    refactor(ui): normalize CRUD page structure

--- resources/views/sucursals/create.blade.php
+++ resources/views/sucursals/create.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
+@section('title', 'Nueva sucursal | ' . config('app.name'))
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                    Crear Sucursal
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-page-header
+        title="Nueva sucursal"
+        subtitle="Registre una sucursal operativa."
+        icon="fas fa-store">
+        <a href="{{ route('sucursal.index') }}" class="btn btn-default">
+            <i class="fas fa-arrow-left"></i>
+            Volver
+        </a>
+    </x-page-header>
 
     <div class="content px-3">
 
         @include('adminlte-templates::common.errors')
 
-        <div class="card">
+        <div class="card sm-form-card">
 
             {!! Form::open(['route' => 'sucursal.store', 'class' => 'confirm-submit']) !!}
 
@@ -30,9 +30,9 @@
 
             </div>
 
-            <div class="card-footer">
-                {!! Form::submit('Guardar', ['class' => 'btn btn-success']) !!}
-                <a href="{{ route('sucursal.index') }}" class="btn btn-primary"> Cancelar </a>
+            <div class="card-footer d-flex justify-content-end">
+                {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('sucursal.index') }}" class="btn btn-default ml-2">Cancelar</a>
             </div>
 
             {!! Form::close() !!}
